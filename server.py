2a03f13b447cfc9a3158a1af725ce6e4297fdab4
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-11-02 21:54:14 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import (Flask,render_template)
 
 app = Flask(__name__, static_folder='./assets', template_folder='./templates')
 
-# app.run(debug=True, use_debugger=False, use_reloader=False)
 
 @app.route('/')
 def render_index():
