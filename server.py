a04533d39f22c51a877f522601e7ab89970630ff
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-11-03 15:10:42 +0100

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import (Flask,render_template)
-
-app = Flask(__name__, static_folder='./assets', template_folder='./templates')
-
-
-@app.route('/')
-def render_index():
-    return render_template("index.jinja",name="home")
-
