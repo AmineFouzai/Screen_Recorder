938ed4243f06b515f543a6727980175446f5a7a7
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-12-12 20:14:12 +0100

    Update recorder.py

--- src/ttos_r/recorder.py
+++ src/ttos_r/recorder.py
@@ -3,8 +3,13 @@ import numpy as np
 import pyautogui
 from tkinter import *
 from multiprocessing import Process
+import multiprocessing as mp
 from time import time
 from random import randint
+import pyaudio
+import wave
+
+
 
 def update_time_text():
     if (state):
@@ -30,14 +35,32 @@ def increment():
     global state
     state = True
 
-def record():
+def record_video():
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 2
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 3
+    filename = "output.wav"
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=sample_format,
+                        channels=channels,
+                        rate=fs,
+                        frames_per_buffer=chunk,
+                        input=True)
+    frames= []
+    #video section
     output = "video.avi"
     img = pyautogui.screenshot()
     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
     height,width,_= img.shape
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(output, fourcc, 20.0, (width, height))
+
     while True:
+        data = stream.read(chunk)
+        frames.append(data)
         lbl.config(text=str(randint(0,100)))
         img = pyautogui.screenshot()
         frame = np.array(img)
@@ -48,12 +71,27 @@ def record():
             break
     cv2.destroyAllWindows()
     out.release()
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+  
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 
-process=Process(target=record)
+
+
+process=Process(target=record_video)
+
 
 def terminate():
     process.terminate()
+  
     quit()
 
 
@@ -61,6 +99,7 @@ def clicked():
 
     btn.config(text="Stop")
     btn.config(command=terminate)
+  
     process.start()
     increment()
    
@@ -76,6 +115,7 @@ btn = Button(window, text="Record", command=clicked,bg="Black",fg="red", height 
 btn.place(relx=0.5, rely=0.5, anchor=CENTER)
 lbl = Label(window, text="00:00:00",font=("Helvetica", 32))
 lbl.pack()
+
 
 if __name__ == "__main__":
     update_time_text()
